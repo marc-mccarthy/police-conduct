6f3c95d81c4dcc793fbad8c1a9d5b7bc62629d76
Pete Hall <pete.mack.hall@gmail.com>
2022-08-21 15:58:57 -0500

    Created interaction details interface and hooks/event handlers

--- src/components/AddReport/InteractionDetails/InteractionDetails.jsx
+++ src/components/AddReport/InteractionDetails/InteractionDetails.jsx
@@ -1,27 +1,59 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import { Button } from '@mui/material';
+import { Button, setRef } from '@mui/material';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 
 function InteractionDetails() {
 
   const dispatch = useDispatch();
+  const history = useHistory();
 
+  const [interactionDate, setInteractionDate] = useState('');
+  const [interactionTime, setInteractionTime] = useState('');
+  const [interactionLocation, setInteractionLocation] = useState('');
+  const [referenceNumber, setReferenceNumber] = useState('');
 
   useEffect(() => {
-    console.log();
-  }, []);
+    console.log(interactionDate, interactionTime, interactionLocation, referenceNumber);
+  }, [interactionDate, interactionTime, interactionLocation, referenceNumber]);
 
+  const back = () => {
+    history.push('/officer-details');
+  };
+
+  const handleDate = (e) => {
+    setInteractionDate(e.target.value);
+  };
+
+  const handleLocation = (e) => {
+    setInteractionLocation(e.target.value);
+  };
+
+  const handleTime = (e) => {
+    setInteractionTime(e.target.value);
+  };
+
+  const handleReferenceNumber = (e) => {
+    setReferenceNumber(e.target.value);
+  };
 
   const next = () => {
     // create new object
-
+    let newDetails = {
+      interaction_date: interactionDate,
+      interaction_time: interactionTime,
+      interaction_location: interactionLocation,
+      reference_number: referenceNumber
+    };
+    console.log(newDetails);
     // send dispatch
-
-    // TODO: history.push to next page
+    dispatch({type: 'INTERACTION_DETAILS', payload: newDetails});
+    // history.push to next page
+    history.push('/description')
   }
 
   return (
@@ -31,9 +63,19 @@ function InteractionDetails() {
 
         <h2>Interaction Details</h2>
 
-        <p>First thing:</p>
+        <p>Date of interaction:</p>
+        <input className='report-input' type="date" onChange={handleDate}></input>
 
-        <Button>BACK</Button>
+        <p>Time of interaction:</p>
+        <input className='report-input' onChange={handleLocation}></input>
+
+        <p>Location of interaction:</p>
+        <input className='report-input' onChange={handleTime}></input>
+
+        <p>Incident reference number (if you have one):</p>
+        <input className='report-input' onChange={handleReferenceNumber}></input>
+
+        <Button onClick={back}>BACK</Button>
         <Button className='report-button' onClick={next}>NEXT</Button>
 
       </div>
