8e98fac34f92c2499f0403b0434427a7e1187211
Frank Raso <frankraso91@gmail.com>
2022-08-22 15:10:49 -0400

    Profile,showing  user.ID's reports

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,44 +1,55 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { Box } from '@mui/material';
+import LoadingBar from '../LoadingBar/LoadingBar';
+
 
 
 
 
 function Profile() {
-    const report = useSelector((store) => store.report);
+    const reports = useSelector((store) => store.reports);
     const user = useSelector((store) => store.user);
     const history = useHistory();
     const dispatch = useDispatch();
 
-    // useEffect(() => {
-    //     dispatch({ type: 'FETCH_REPORTS' });
-    // }, []);
-    // const gearR = (id) => {
-    //     history.push(`/reports/${id}`)
-    // }
+    useEffect(() => {
+        dispatch({ type: 'FETCH_REPORTS' });
+    }, []);
+    const details = (id) => {
+        history.push(`/reports/${id}`)
+    }
 
     return (
         <div className="container">
-            <h2>Welcome {user.username} !</h2>
-            <h3>Your Reports :</h3>
-            <p>Reports will be here!</p>
-
-            {/* <div className='ReportList'>
-                {report.map(eachReport => {
-                    return (
-                        <div className='ReportItem' key={eachReport.id} >
-                            <h3>{eachReport.id}</h3>
-                            <p>{eachReport.interaction_date}</p>
-                            <p>{eachReport.interaction_summary}</p>
-                            <button onClick={() => gearR(eachReport.id)}>Details</button>
-                        </div>
-                    );
+          {reports.length == 0 ? (
+            <Box mt={25} display="flex" justifyContent="center" alignItems="center">
+              <LoadingBar />
+            </Box>
+          ) : (
+            <div>
+              <h2>{user.username}'s Reports :</h2>
+              <div className="ReportList">
+                {reports.map((eachReport) => {
+                  return (
+                    eachReport.userID == user.id && (
+                    <div className="ReportItem" key={eachReport.id}>
+                      <h3>Report # {eachReport.id}</h3>
+                      <p>{eachReport.reporter_first}</p>
+                      <p>Date: {eachReport.interaction_date.substring(0,10)}</p>
+                      <p>{eachReport.interaction_summary.substring(0, 30)}...</p>
+                      <button onClick={() => details(eachReport.id)}>Details</button>
+                    </div>
+                  ));
                 })}
-            </div > */}
-        </div >
-    );
-}
+              </div>
+            </div>
+          )}
+        </div>
+      );
+    }
+    
 
 export default Profile;
