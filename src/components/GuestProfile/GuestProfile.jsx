577e1f7baf36f703b89e3ed225fda95d706ce554
Frank Raso <frankraso91@gmail.com>
2022-08-26 15:17:08 -0400

    Reroute

--- src/components/GuestProfile/GuestProfile.jsx
+++ src/components/GuestProfile/GuestProfile.jsx
@@ -6,7 +6,7 @@ function GuestProfile() {
     <div className="container">
       <h2>Welcome!</h2>
       <p>You are not currently logged in.</p>
-      <p>If you want to save, edit, or delete your reports, you must <a href='/register'>create an account.</a> </p>
+      <p>If you want to save, edit, or delete your reports, you must <a href='/register'>Create account, or Log In</a> </p>
         <p>To continue adding a report without an account <a href='#/start'>click here</a> </p>
     </div>
   );
