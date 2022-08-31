ec249b72f7dc904ebd5e9591a4439ad11f2611b0
Robert Thao <robert.lee.thao@gmail.com>
2022-08-30 19:41:53 -0500

    SideBar buttons can now differentiate between logged in users and guests

--- src/components/SideBar/SidebarData.js
+++ src/components/SideBar/SidebarData.js
@@ -10,24 +10,28 @@ export const SidebarData = [
     title: 'Add Report',
     path: '/start',
     icon: <TbIcons.TbReportMedical />,
-    cName: 'nav-text'
+    cName: 'nav-text',
+    userReq: false
   },
   {
     title: 'View Reports',
     path: '/viewReports',
     icon: <TbIcons.TbReportAnalytics />,
-    cName: 'nav-text'
+    cName: 'nav-text',
+    userReq: false
   },
   {
     title: 'Profile',
     path: '/profile',
     icon: <TbIcons.TbHome2 />,
-    cName: 'nav-text'
+    cName: 'nav-text',
+    userReq: false
   },
   {
     title: 'Log Out',
     path: '/logout',
     icon: <TbIcons.TbLogout />,
-    cName: 'nav-text'
+    cName: 'nav-text',
+    userReq: true
   },
 ];
